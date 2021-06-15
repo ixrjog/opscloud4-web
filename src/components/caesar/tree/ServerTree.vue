c562fe6c3e08fbc592a1aaa57ca9b8a7954f83ad
baiyi <baiyi@xinc818.group>
2021-06-15 09:53:13 +0800

    修改查询参数

--- src/components/caesar/tree/ServerTree.vue
+++ src/components/caesar/tree/ServerTree.vue
@@ -1,24 +1,24 @@
 <template>
-    <el-card style="margin-top: 5px" class="box-card" shadow="never">
-      <div slot="header" class="clearfix">
-        <span><i class="fa fa-list" aria-hidden="true"></i></span>
-        <el-tag style="float: right" disable-transitions>Size: {{ serverSize }}</el-tag>
-      </div>
-      <el-row>
-        <el-input v-model="queryParam.name" placeholder="名称" style="display: inline-block; maxWidth: 200px;"/>
-        <el-select v-model.trim="queryParam.serverGroupTypeId" filterable clearable
-                   remote reserve-keyword placeholder="输入关键词搜组类型" :remote-method="getGroupType" :loading="loading">
-          <el-option
-            v-for="item in groupTypeOptions"
-            :key="item.id"
-            :label="item.name"
-            :value="item.id">
-          </el-option>
-        </el-select>
-        <el-button @click="fetchData" :loading="searching">查询</el-button>
-      </el-row>
-      <el-tree style="margin-top: 5px" :data="serverTree.tree" show-checkbox ref="myServerTree" node-key="id"></el-tree>
-    </el-card>
+  <el-card style="margin-top: 5px" class="box-card" shadow="never">
+    <div slot="header" class="clearfix">
+      <span><i class="fa fa-list" aria-hidden="true"></i></span>
+      <el-tag style="float: right" disable-transitions>Size: {{ serverSize }}</el-tag>
+    </div>
+    <el-row>
+      <el-input v-model="queryParam.name" placeholder="名称" style="display: inline-block; maxWidth: 200px;"/>
+      <el-select v-model.trim="queryParam.serverGroupTypeId" filterable clearable
+                 remote reserve-keyword placeholder="输入关键词搜组类型" :remote-method="getGroupType" :loading="loading">
+        <el-option
+          v-for="item in groupTypeOptions"
+          :key="item.id"
+          :label="item.name"
+          :value="item.id">
+        </el-option>
+      </el-select>
+      <el-button @click="fetchData" :loading="searching">查询</el-button>
+    </el-row>
+    <el-tree style="margin-top: 5px" :data="serverTree.tree" show-checkbox ref="myServerTree" node-key="id"></el-tree>
+  </el-card>
 </template>
 
 <script>
@@ -64,9 +64,8 @@ export default {
     getUuid () {
       return this.serverTree.uuid
     },
-    fetchData () {
+    async fetchData () {
       this.searching = true
-      this.uuid = ''
       const requestBody = {
         ...this.queryParam
       }
