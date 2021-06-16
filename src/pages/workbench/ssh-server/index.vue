afdea03480739404c665a526d9f7eefa87b0235f
修远 <xiuyuan@xinc818.group>
2021-06-16 17:39:02 +0800

    安装markdown-it-vue

--- src/pages/workbench/ssh-server/index.vue
+++ src/pages/workbench/ssh-server/index.vue
@@ -1,14 +1,12 @@
 <template>
   <d2-container>
-    <d2-markdown v-if="doc !== null" :source="doc.content" highlight/>
+    <markdown-it-vue v-if="doc !== null" :content="doc.content" :options="options"></markdown-it-vue>
   </d2-container>
 </template>
 
 <script>
-
-// import doc from '@/static/md/ssh-server.md'
-import Highlight from '@/components/d2-highlight'
-
+import MarkdownItVue from 'markdown-it-vue'
+import 'markdown-it-vue/dist/markdown-it-vue.css'
 import { PREVIEW_DOCUMENT } from '@/api/modules/sys/sys.doc.api.js'
 
 const documentKey = 'SSH_SERVER_README'
@@ -21,11 +19,17 @@ export default {
       documentKey: documentKey,
       dict: {
         sshServerHost: window.location.hostname
+      },
+      options: {
+        markdownIt: {
+          html: true,
+          linkify: true
+        }
       }
     }
   },
   components: {
-    Highlight
+    MarkdownItVue
   },
   mounted () {
     this.fetchData()
