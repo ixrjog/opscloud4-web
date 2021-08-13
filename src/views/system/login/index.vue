2547686090c04fd5dfec468e564ad36d4e7a0e22
白衣 <liangjian@xinc818.group>
2021-08-13 11:35:07 +0800

    增加云词

--- src/views/system/login/index.vue
+++ src/views/system/login/index.vue
@@ -16,14 +16,14 @@
         flex="dir:top main:justify cross:stretch box:justify">
         <div class="page-login--content-header">
           <p class="page-login--content-header-motto">
-            抱怨身处黑暗，不如提灯前行
+            {{wordcloud}}
           </p>
         </div>
         <div
           class="page-login--content-main"
           flex="dir:top main:center cross:center">
           <!-- logo -->
-<!--          <img class="page-login&#45;&#45;logo" src="./image/logo@2x.png">-->
+          <!--          <img class="page-login&#45;&#45;logo" src="./image/logo@2x.png">-->
           <div style="font-size: 30px; color: #2a6cd9;margin-bottom: 10px">OPSCLOUD 4 IaC</div>
           <!-- form -->
           <div class="page-login--form">
@@ -50,16 +50,16 @@
                     <i slot="prepend" class="fas fa-keyboard"></i>
                   </el-input>
                 </el-form-item>
-<!--                <el-form-item prop="code">-->
-<!--                  <el-input-->
-<!--                    type="text"-->
-<!--                    v-model="formLogin.code"-->
-<!--                    placeholder="验证码">-->
-<!--                    <template slot="append">-->
-<!--                      <img class="login-code" src="./image/login-code.png">-->
-<!--                    </template>-->
-<!--                  </el-input>-->
-<!--                </el-form-item>-->
+                <!--                <el-form-item prop="code">-->
+                <!--                  <el-input-->
+                <!--                    type="text"-->
+                <!--                    v-model="formLogin.code"-->
+                <!--                    placeholder="验证码">-->
+                <!--                    <template slot="append">-->
+                <!--                      <img class="login-code" src="./image/login-code.png">-->
+                <!--                    </template>-->
+                <!--                  </el-input>-->
+                <!--                </el-form-item>-->
                 <el-button
                   size="default"
                   @click="submit"
@@ -76,9 +76,9 @@
               <span>注册用户</span>
             </p>
             <!-- quick login -->
-<!--            <el-button class="page-login&#45;&#45;quick" size="default" type="info" @click="dialogVisible = true">-->
-<!--              快速选择用户（测试功能）-->
-<!--            </el-button>-->
+            <!--            <el-button class="page-login&#45;&#45;quick" size="default" type="info" @click="dialogVisible = true">-->
+            <!--              快速选择用户（测试功能）-->
+            <!--            </el-button>-->
           </div>
         </div>
         <div class="page-login--content-footer">
@@ -90,35 +90,35 @@
               {{ language.label }}
             </a>
           </p>
-<!--          <p class="page-login&#45;&#45;content-footer-copyright">-->
-<!--            Copyright-->
-<!--            <d2-icon name="copyright"/>-->
-<!--            2018 D2 Projects 开源组织出品-->
-<!--            <a href="https://github.com/FairyEver">-->
-<!--              @FairyEver-->
-<!--            </a>-->
-<!--          </p>-->
-<!--          <p class="page-login&#45;&#45;content-footer-options">-->
-<!--            <a href="#">帮助</a>-->
-<!--            <a href="#">隐私</a>-->
-<!--            <a href="#">条款</a>-->
-<!--          </p>-->
+          <!--          <p class="page-login&#45;&#45;content-footer-copyright">-->
+          <!--            Copyright-->
+          <!--            <d2-icon name="copyright"/>-->
+          <!--            2018 D2 Projects 开源组织出品-->
+          <!--            <a href="https://github.com/FairyEver">-->
+          <!--              @FairyEver-->
+          <!--            </a>-->
+          <!--          </p>-->
+          <!--          <p class="page-login&#45;&#45;content-footer-options">-->
+          <!--            <a href="#">帮助</a>-->
+          <!--            <a href="#">隐私</a>-->
+          <!--            <a href="#">条款</a>-->
+          <!--          </p>-->
         </div>
       </div>
     </div>
-<!--    <el-dialog-->
-<!--      title="快速选择用户"-->
-<!--      :visible.sync="dialogVisible"-->
-<!--      width="400px">-->
-<!--      <el-row :gutter="10" style="margin: -20px 0px -10px 0px;">-->
-<!--        <el-col v-for="(user, index) in users" :key="index" :span="8">-->
-<!--          <div class="page-login&#45;&#45;quick-user" @click="handleUserBtnClick(user)">-->
-<!--            <d2-icon name="user-circle-o"/>-->
-<!--            <span>{{user.name}}</span>-->
-<!--          </div>-->
-<!--        </el-col>-->
-<!--      </el-row>-->
-<!--    </el-dialog>-->
+    <!--    <el-dialog-->
+    <!--      title="快速选择用户"-->
+    <!--      :visible.sync="dialogVisible"-->
+    <!--      width="400px">-->
+    <!--      <el-row :gutter="10" style="margin: -20px 0px -10px 0px;">-->
+    <!--        <el-col v-for="(user, index) in users" :key="index" :span="8">-->
+    <!--          <div class="page-login&#45;&#45;quick-user" @click="handleUserBtnClick(user)">-->
+    <!--            <d2-icon name="user-circle-o"/>-->
+    <!--            <span>{{user.name}}</span>-->
+    <!--          </div>-->
+    <!--        </el-col>-->
+    <!--      </el-row>-->
+    <!--    </el-dialog>-->
   </div>
 </template>
 
@@ -128,12 +128,27 @@ import { mapActions } from 'vuex'
 import localeMixin from '@/locales/mixin.js'
 import ui from '@/libs/util.ui'
 
+const wordcloud = [
+  '抱怨身处黑暗 不如提灯前行',
+  '大鹏一日同风起 扶摇直上直上九万里',
+  '莫忘少年凌云志 曾许人间第一流',
+  '同是风华正茂 怎能甘拜下风',
+  '乾坤未定 你我皆是黑马',
+  '在繁华中自律 在落魄中自愈',
+  '越痛越不动神色 越苦越保持沉默',
+  '纵使无人伴身旁 也要一展少年狂',
+  '你所看到的惊艳 都曾被平庸历练',
+  '金麟岂是池中物 一遇风云上九霄',
+  '人有冲天之志 非运不可自通'
+]
+
 export default {
   mixins: [
     localeMixin
   ],
   data () {
     return {
+      wordcloud: wordcloud[Math.floor((Math.random() * wordcloud.length))],
       timeInterval: null,
       time: dayjs().format('HH:mm:ss'),
       // 快速选择用户
@@ -242,249 +257,287 @@ export default {
 </script>
 
 <style lang="scss">
-.page-login {
-  @extend %unable-select;
-  $backgroundColor: #F0F2F5;
-  // ---
-  background-color: $backgroundColor;
-  height: 100%;
-  position: relative;
-  // 层
-  .page-login--layer {
-    @extend %full;
-    overflow: auto;
-  }
-  .page-login--layer-area {
-    overflow: hidden;
-  }
-  // 时间
-  .page-login--layer-time {
-    font-size: 24em;
-    font-weight: bold;
-    color: rgba(0, 0, 0, 0.03);
-    overflow: hidden;
-  }
-  // 登陆页面控件的容器
-  .page-login--content {
+  .page-login {
+    @extend %unable-select;
+    $backgroundColor: #F0F2F5;
+    // ---
+    background-color: $backgroundColor;
     height: 100%;
-    min-height: 500px;
-  }
-  // header
-  .page-login--content-header {
-    padding: 1em 0;
-    .page-login--content-header-motto {
-      margin: 0px;
-      padding: 0px;
-      color: $color-text-normal;
-      text-align: center;
-      font-size: 12px;
+    position: relative;
+    // 层
+    .page-login--layer {
+      @extend %full;
+      overflow: auto;
     }
-  }
-  // main
-  .page-login--logo {
-    width: 240px;
-    margin-bottom: 2em;
-    margin-top: -2em;
-  }
-  // 登录表单
-  .page-login--form {
-    width: 280px;
-    // 卡片
-    .el-card {
-      margin-bottom: 15px;
+
+    .page-login--layer-area {
+      overflow: hidden;
     }
-    // 登录按钮
-    .button-login {
-      width: 100%;
-    }
-    // 输入框左边的图表区域缩窄
-    .el-input-group__prepend {
-      padding: 0px 14px;
-    }
-    .login-code {
-      height: 40px - 2px;
-      display: block;
-      margin: 0px -20px;
-      border-top-right-radius: 2px;
-      border-bottom-right-radius: 2px;
-    }
-    // 登陆选项
-    .page-login--options {
-      margin: 0px;
-      padding: 0px;
-      font-size: 14px;
-      color: $color-primary;
-      margin-bottom: 15px;
+
+    // 时间
+    .page-login--layer-time {
+      font-size: 24em;
       font-weight: bold;
+      color: rgba(0, 0, 0, 0.03);
+      overflow: hidden;
     }
-    .page-login--quick {
-      width: 100%;
+
+    // 登陆页面控件的容器
+    .page-login--content {
+      height: 100%;
+      min-height: 500px;
     }
-  }
-  // 快速选择用户面板
-  .page-login--quick-user {
-    @extend %flex-center-col;
-    padding: 10px 0px;
-    border-radius: 4px;
-    &:hover {
-      background-color: $color-bg;
+
+    // header
+    .page-login--content-header {
+      padding: 1em 0;
+
+      .page-login--content-header-motto {
+        margin: 0px;
+        padding: 0px;
+        color: $color-text-normal;
+        text-align: center;
+        font-size: 12px;
+      }
+    }
+
+    // main
+    .page-login--logo {
+      width: 240px;
+      margin-bottom: 2em;
+      margin-top: -2em;
+    }
+
+    // 登录表单
+    .page-login--form {
+      width: 280px;
+      // 卡片
+      .el-card {
+        margin-bottom: 15px;
+      }
+
+      // 登录按钮
+      .button-login {
+        width: 100%;
+      }
+
+      // 输入框左边的图表区域缩窄
+      .el-input-group__prepend {
+        padding: 0px 14px;
+      }
+
+      .login-code {
+        height: 40px - 2px;
+        display: block;
+        margin: 0px -20px;
+        border-top-right-radius: 2px;
+        border-bottom-right-radius: 2px;
+      }
+
+      // 登陆选项
+      .page-login--options {
+        margin: 0px;
+        padding: 0px;
+        font-size: 14px;
+        color: $color-primary;
+        margin-bottom: 15px;
+        font-weight: bold;
+      }
+
+      .page-login--quick {
+        width: 100%;
+      }
+    }
+
+    // 快速选择用户面板
+    .page-login--quick-user {
+      @extend %flex-center-col;
+      padding: 10px 0px;
+      border-radius: 4px;
+
+      &:hover {
+        background-color: $color-bg;
+
+        i {
+          color: $color-text-normal;
+        }
+
+        span {
+          color: $color-text-normal;
+        }
+      }
+
       i {
-        color: $color-text-normal;
+        font-size: 36px;
+        color: $color-text-sub;
       }
+
       span {
-        color: $color-text-normal;
+        font-size: 12px;
+        margin-top: 10px;
+        color: $color-text-sub;
       }
     }
-    i {
-      font-size: 36px;
-      color: $color-text-sub;
-    }
-    span {
-      font-size: 12px;
-      margin-top: 10px;
-      color: $color-text-sub;
-    }
-  }
-  // footer
-  .page-login--content-footer {
-    padding: 1em 0;
-    .page-login--content-footer-locales {
-      padding: 0px;
-      margin: 0px;
-      margin-bottom: 15px;
-      font-size: 12px;
-      line-height: 12px;
-      text-align: center;
-      color: $color-text-normal;
-      a {
+
+    // footer
+    .page-login--content-footer {
+      padding: 1em 0;
+
+      .page-login--content-footer-locales {
+        padding: 0px;
+        margin: 0px;
+        margin-bottom: 15px;
+        font-size: 12px;
+        line-height: 12px;
+        text-align: center;
         color: $color-text-normal;
-        margin: 0 .5em;
-        &:hover {
-          color: $color-text-main;
+
+        a {
+          color: $color-text-normal;
+          margin: 0 .5em;
+
+          &:hover {
+            color: $color-text-main;
+          }
+        }
+      }
+
+      .page-login--content-footer-copyright {
+        padding: 0px;
+        margin: 0px;
+        margin-bottom: 10px;
+        font-size: 12px;
+        line-height: 12px;
+        text-align: center;
+        color: $color-text-normal;
+
+        a {
+          color: $color-text-normal;
+        }
+      }
+
+      .page-login--content-footer-options {
+        padding: 0px;
+        margin: 0px;
+        font-size: 12px;
+        line-height: 12px;
+        text-align: center;
+
+        a {
+          color: $color-text-normal;
+          margin: 0 1em;
         }
       }
     }
-    .page-login--content-footer-copyright {
-      padding: 0px;
-      margin: 0px;
-      margin-bottom: 10px;
-      font-size: 12px;
-      line-height: 12px;
-      text-align: center;
-      color: $color-text-normal;
-      a {
-        color: $color-text-normal;
-      }
-    }
-    .page-login--content-footer-options {
-      padding: 0px;
-      margin: 0px;
-      font-size: 12px;
-      line-height: 12px;
-      text-align: center;
-      a {
-        color: $color-text-normal;
-        margin: 0 1em;
-      }
-    }
-  }
-  // 背景
-  .circles {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    margin: 0px;
-    padding: 0px;
-    li {
+
+    // 背景
+    .circles {
       position: absolute;
-      display: block;
-      list-style: none;
-      width: 20px;
-      height: 20px;
-      background: #FFF;
-      animation: animate 25s linear infinite;
-      bottom: -200px;
-      @keyframes animate {
-        0%{
-          transform: translateY(0) rotate(0deg);
-          opacity: 1;
-          border-radius: 0;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      margin: 0px;
+      padding: 0px;
+
+      li {
+        position: absolute;
+        display: block;
+        list-style: none;
+        width: 20px;
+        height: 20px;
+        background: #FFF;
+        animation: animate 25s linear infinite;
+        bottom: -200px;
+        @keyframes animate {
+          0% {
+            transform: translateY(0) rotate(0deg);
+            opacity: 1;
+            border-radius: 0;
+          }
+          100% {
+            transform: translateY(-1000px) rotate(720deg);
+            opacity: 0;
+            border-radius: 50%;
+          }
         }
-        100%{
-          transform: translateY(-1000px) rotate(720deg);
-          opacity: 0;
-          border-radius: 50%;
+
+        &:nth-child(1) {
+          left: 15%;
+          width: 80px;
+          height: 80px;
+          animation-delay: 0s;
         }
-      }
-      &:nth-child(1) {
-        left: 15%;
-        width: 80px;
-        height: 80px;
-        animation-delay: 0s;
-      }
-      &:nth-child(2) {
-        left: 5%;
-        width: 20px;
-        height: 20px;
-        animation-delay: 2s;
-        animation-duration: 12s;
-      }
-      &:nth-child(3) {
-        left: 70%;
-        width: 20px;
-        height: 20px;
-        animation-delay: 4s;
-      }
-      &:nth-child(4) {
-        left: 40%;
-        width: 60px;
-        height: 60px;
-        animation-delay: 0s;
-        animation-duration: 18s;
-      }
-      &:nth-child(5) {
-        left: 65%;
-        width: 20px;
-        height: 20px;
-        animation-delay: 0s;
-      }
-      &:nth-child(6) {
-        left: 75%;
-        width: 150px;
-        height: 150px;
-        animation-delay: 3s;
-      }
-      &:nth-child(7) {
-        left: 35%;
-        width: 200px;
-        height: 200px;
-        animation-delay: 7s;
-      }
-      &:nth-child(8) {
-        left: 50%;
-        width: 25px;
-        height: 25px;
-        animation-delay: 15s;
-        animation-duration: 45s;
-      }
-      &:nth-child(9) {
-        left: 20%;
-        width: 15px;
-        height: 15px;
-        animation-delay: 2s;
-        animation-duration: 35s;
-      }
-      &:nth-child(10) {
-        left: 85%;
-        width: 150px;
-        height: 150px;
-        animation-delay: 0s;
-        animation-duration: 11s;
+
+        &:nth-child(2) {
+          left: 5%;
+          width: 20px;
+          height: 20px;
+          animation-delay: 2s;
+          animation-duration: 12s;
+        }
+
+        &:nth-child(3) {
+          left: 70%;
+          width: 20px;
+          height: 20px;
+          animation-delay: 4s;
+        }
+
+        &:nth-child(4) {
+          left: 40%;
+          width: 60px;
+          height: 60px;
+          animation-delay: 0s;
+          animation-duration: 18s;
+        }
+
+        &:nth-child(5) {
+          left: 65%;
+          width: 20px;
+          height: 20px;
+          animation-delay: 0s;
+        }
+
+        &:nth-child(6) {
+          left: 75%;
+          width: 150px;
+          height: 150px;
+          animation-delay: 3s;
+        }
+
+        &:nth-child(7) {
+          left: 35%;
+          width: 200px;
+          height: 200px;
+          animation-delay: 7s;
+        }
+
+        &:nth-child(8) {
+          left: 50%;
+          width: 25px;
+          height: 25px;
+          animation-delay: 15s;
+          animation-duration: 45s;
+        }
+
+        &:nth-child(9) {
+          left: 20%;
+          width: 15px;
+          height: 15px;
+          animation-delay: 2s;
+          animation-duration: 35s;
+        }
+
+        &:nth-child(10) {
+          left: 85%;
+          width: 150px;
+          height: 150px;
+          animation-delay: 0s;
+          animation-duration: 11s;
+        }
       }
     }
   }
-}
 </style>
