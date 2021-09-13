b175adee16e50fbe42d5d1160dea5b37ea4c7665
白衣 <ixrjog@qq.com>
2021-09-13 13:58:05 +0800

    优化用户名称tag

--- src/components/opscloud/common/tag/UsersTag.vue
+++ src/components/opscloud/common/tag/UsersTag.vue
@@ -2,7 +2,7 @@
   <span class="tag-group">
     <span v-for="item in users" :key="item.id">
         <el-tag style="margin-left: 5px">
-          {{ item.username }}&lt;{{ item.name !== '' ? item.name + ':' : '' }}{{ item.displayName }}&gt;
+          {{ item.username }}&lt;{{ item.name !== null && item.name !== ''  ? item.name + ':' : '' }}{{ item.displayName }}&gt;
         </el-tag>
     </span>
   </span>
