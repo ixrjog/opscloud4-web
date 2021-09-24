8ea09f52d094c57c626c35cea981a6f43b1c07f2
白衣 <ixrjog@qq.com>
2021-09-24 16:58:43 +0800

    容器展示启动时间

--- src/components/opscloud/terminal/kubernetes/ApplicationKubernetesSelector.vue
+++ src/components/opscloud/terminal/kubernetes/ApplicationKubernetesSelector.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <el-row :gutter="24" style="margin-bottom: 5px; margin-left: 0px;">
-      <el-input v-model.trim="queryParam.queryName" placeholder="输入关键字查询"/>
-      <el-button @click="fetchData" class="button">查询</el-button>
-    </el-row>
-    <el-table :data="table.data" style="width: 100%" v-loading="table.loading">
-      <el-table-column prop="name" label="应用名称" width="200"></el-table-column>
-      <el-table-column prop="resources" label="无状态">
-        <template slot-scope="scope" v-if="scope.row.resources !== null && scope.row.resources.length > 0">
+    <div>
+        <el-row :gutter="24" style="margin-bottom: 5px; margin-left: 0px;">
+            <el-input v-model.trim="queryParam.queryName" placeholder="输入关键字查询"/>
+            <el-button @click="fetchData" class="button">查询</el-button>
+        </el-row>
+        <el-table :data="table.data" style="width: 100%" v-loading="table.loading">
+            <el-table-column prop="name" label="应用名称" width="200"></el-table-column>
+            <el-table-column prop="resources" label="无状态">
+                <template slot-scope="scope" v-if="scope.row.resources !== null && scope.row.resources.length > 0">
           <span v-for="resource in scope.row.resources" :key="resource.id">
             <el-card shadow="never">
                <div style="margin-top: -10px">
@@ -15,11 +15,11 @@
                   <span v-if="resource.instance !== null">{{ resource.instance.instanceName}}/</span>{{ resource.name}}
                   <el-button style="float: right; padding: 3px 0" type="text"
                              @click="handleLog(resource)">Log
-<!--                    <i class="far fa-file-alt"></i>-->
+                      <!--                    <i class="far fa-file-alt"></i>-->
                   </el-button>
                   <el-button style="float: right; padding: 3px 0" type="text"
                              @click="handleTerminal(resource)">Terminal
-<!--                    <i class="fas fa-terminal"></i>-->
+                      <!--                    <i class="fas fa-terminal"></i>-->
                   </el-button>
                </div>
                <el-divider/>
@@ -30,21 +30,21 @@
                  </el-tag>
                  <span style="margin-left: 5px;display: inline-block" v-for="container in pod.children"
                        :key="container.asset.name">
-                    <el-tag><i style="margin-right: 2px" class="fab fa-docker"></i>{{ container.asset.name}}<el-checkbox
-                      style="margin-left: 5px" v-model="container.checked"></el-checkbox></el-tag>
+                    <el-tag><i style="margin-right: 2px" class="fab fa-docker"></i>{{ container.asset.name}} [ {{pod.properties.startTime}} ]<el-checkbox
+                            style="margin-left: 5px" v-model="container.checked"></el-checkbox></el-tag>
                  </span>
                </div>
             </el-card>
           </span>
-        </template>
-      </el-table-column>
-      <!--      <el-table-column prop="env" label="环境" width="80">-->
-      <!--        <template slot-scope="scope">-->
-      <!--          <env-tag :env="scope.row.env"></env-tag>-->
-      <!--        </template>-->
-      <!--      </el-table-column>-->
-    </el-table>
-  </div>
+                </template>
+            </el-table-column>
+            <!--      <el-table-column prop="env" label="环境" width="80">-->
+            <!--        <template slot-scope="scope">-->
+            <!--          <env-tag :env="scope.row.env"></env-tag>-->
+            <!--        </template>-->
+            <!--      </el-table-column>-->
+        </el-table>
+    </div>
 </template>
 
 <script>
@@ -148,25 +148,25 @@ export default {
 
 <style scoped>
 
-  .el-divider--horizontal {
-    display: block;
-    height: 1px;
-    width: 100%;
-    margin: 1px 0;
-    margin-bottom: 8px;
-  }
+    .el-divider--horizontal {
+        display: block;
+        height: 1px;
+        width: 100%;
+        margin: 1px 0;
+        margin-bottom: 8px;
+    }
 
-  .el-input {
-    display: inline-block;
-    max-width: 200px;
-  }
+    .el-input {
+        display: inline-block;
+        max-width: 200px;
+    }
 
-  .el-select {
-    margin-left: 5px;
-  }
+    .el-select {
+        margin-left: 5px;
+    }
 
-  .el-button {
-    margin-left: 5px;
-  }
+    .el-button {
+        margin-left: 5px;
+    }
 
 </style>
