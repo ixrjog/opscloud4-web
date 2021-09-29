5b45581e440f98a5bbb12e3b394d4b7773daf912
白衣 <ixrjog@qq.com>
2021-09-29 14:05:12 +0800

    容器堡垒机: 未选中容器，不执行操作

--- src/components/opscloud/terminal/kubernetes/ApplicationKubernetesSelector.vue
+++ src/components/opscloud/terminal/kubernetes/ApplicationKubernetesSelector.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <el-row :gutter="24" style="margin-bottom: 5px; margin-left: 0px;">
-            <el-input v-model.trim="queryParam.queryName" placeholder="输入关键字查询"/>
-            <el-button @click="fetchData" class="button">查询</el-button>
-        </el-row>
-        <el-table :data="table.data" style="width: 100%" v-loading="table.loading">
-            <el-table-column prop="name" label="应用名称" width="200"></el-table-column>
-            <el-table-column prop="resources" label="无状态">
-                <template slot-scope="scope" v-if="scope.row.resources !== null && scope.row.resources.length > 0">
+  <div>
+    <el-row :gutter="24" style="margin-bottom: 5px; margin-left: 0px;">
+      <el-input v-model.trim="queryParam.queryName" placeholder="输入关键字查询"/>
+      <el-button @click="fetchData" class="button">查询</el-button>
+    </el-row>
+    <el-table :data="table.data" style="width: 100%" v-loading="table.loading">
+      <el-table-column prop="name" label="应用名称" width="200"></el-table-column>
+      <el-table-column prop="resources" label="无状态">
+        <template slot-scope="scope" v-if="scope.row.resources !== null && scope.row.resources.length > 0">
           <span v-for="resource in scope.row.resources" :key="resource.id">
             <el-card shadow="never">
                <div style="margin-top: -10px">
@@ -15,41 +15,45 @@
                   <span v-if="resource.instance !== null">{{ resource.instance.instanceName}}/</span>{{ resource.name}}
                   <el-button style="float: right; padding: 3px 0" type="text"
                              @click="handleLog(resource)">Log
-                      <!--                    <i class="far fa-file-alt"></i>-->
+                    <!--                    <i class="far fa-file-alt"></i>-->
                   </el-button>
                   <el-button style="float: right; padding: 3px 0" type="text"
                              @click="handleTerminal(resource)">Terminal
-                      <!--                    <i class="fas fa-terminal"></i>-->
+                    <!--                    <i class="fas fa-terminal"></i>-->
                   </el-button>
                </div>
                <el-divider/>
-                <!--Pod-->
+              <!--Pod-->
                <div v-for="pod in resource.assetContainers" :key="pod.asset.name">
                  <el-tooltip :content="pod.properties.image" placement="bottom" effect="light">
                    <el-tag style="margin-left: 10px;vertical-align:top;">
-                     <i class="fab fa-artstation"></i>{{ pod.asset.name }}
+                     <i class="fab fa-artstation" style="margin-right: 2px"></i>{{ pod.asset.name }}
                      <span style="margin-left: 5px;color: #67C23A">[{{ pod.properties.phase }}]</span>
                      <span style="margin-left: 5px">[ 启动时间: {{pod.properties.startTime}} / 重启次数: {{pod.properties.restartCount }} ]</span>
                    </el-tag>
                  </el-tooltip>
-                   <!--容器-->
-                 <span style="margin-left: 5px;display: inline-block" v-for="container in pod.children"
-                       :key="container.asset.name">
-                           <el-tag><i style="margin-right: 2px" class="fab fa-docker"></i>{{ container.asset.name}}<el-checkbox
-                                   style="margin-left: 5px" v-model="container.checked"></el-checkbox></el-tag>
+                 <!--容器-->
+                 <span v-for="container in pod.children"
+                       :key="container.asset.name"
+                       style="margin-left: 5px;display: inline-block">
+                           <el-tag>
+                             <el-checkbox style="margin-left: 5px" v-model="container.checked"></el-checkbox>
+                             <i style="margin-left: 5px;margin-right: 2px" class="fab fa-docker"></i>
+                             {{ container.asset.name}}
+                           </el-tag>
                  </span>
                </div>
             </el-card>
           </span>
-                </template>
-            </el-table-column>
-            <!--      <el-table-column prop="env" label="环境" width="80">-->
-            <!--        <template slot-scope="scope">-->
-            <!--          <env-tag :env="scope.row.env"></env-tag>-->
-            <!--        </template>-->
-            <!--      </el-table-column>-->
-        </el-table>
-    </div>
+        </template>
+      </el-table-column>
+      <!--      <el-table-column prop="env" label="环境" width="80">-->
+      <!--        <template slot-scope="scope">-->
+      <!--          <env-tag :env="scope.row.env"></env-tag>-->
+      <!--        </template>-->
+      <!--      </el-table-column>-->
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -113,6 +117,10 @@ export default {
           pods.push(pod)
         }
       }
+      if (pods.length === 0) {
+        this.$message.warning('未选中容器，无法执行当前操作！')
+        return
+      }
       const loginParam = {
         state: 'LOGIN',
         sessionType: type,
@@ -153,25 +161,25 @@ export default {
 
 <style scoped>
 
-    .el-divider--horizontal {
-        display: block;
-        height: 1px;
-        width: 100%;
-        margin: 1px 0;
-        margin-bottom: 8px;
-    }
+  .el-divider--horizontal {
+    display: block;
+    height: 1px;
+    width: 100%;
+    margin: 1px 0;
+    margin-bottom: 8px;
+  }
 
-    .el-input {
-        display: inline-block;
-        max-width: 200px;
-    }
+  .el-input {
+    display: inline-block;
+    max-width: 200px;
+  }
 
-    .el-select {
-        margin-left: 5px;
-    }
+  .el-select {
+    margin-left: 5px;
+  }
 
-    .el-button {
-        margin-left: 5px;
-    }
+  .el-button {
+    margin-left: 5px;
+  }
 
 </style>
