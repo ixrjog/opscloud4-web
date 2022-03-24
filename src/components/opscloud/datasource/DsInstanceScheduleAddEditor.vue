f2063779a6374f283cf5b856ef263591a713d705
修远 <448613222@qq.com>
2022-03-24 19:32:33 +0800

    数据源定时任务管理

---
+++ src/components/opscloud/datasource/DsInstanceScheduleAddEditor.vue
@@ -0,0 +1,116 @@
+<template>
+  <el-dialog title="新增数据源实例动态分布式任务" :visible.sync="formStatus.visible" append-to-body width="40%">
+    <el-form :model="scheduleJob" label-width="80px">
+      <el-form-item label="任务类型" required>
+        <el-select v-model="scheduleJob.jobType" placeholder="选择类型">
+          <el-option
+            v-for="item in jobTypeOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="资产类型" required>
+        <el-select v-model="scheduleJob.assetType" placeholder="选择资产类型">
+          <el-option
+            v-for="item in assetTypeOptions"
+            :key="item.assetType"
+            :label="item.assetType"
+            :value="item.assetType">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Cron" required>
+        <el-input v-model="scheduleJob.jobTime" @change="handlerChange">
+          <el-button slot="append" @click="handleCheck()" :icon="cronChecked?'el-icon-success':'el-icon-warning'">
+          </el-button>
+        </el-input>
+        <el-alert title="最近5次执行时间" type="info" v-if="showExecutionTime" style="margin-top: 5px" :closable="false">
+          <div v-for="time in executionTime" :key="time">{{ time }}</div>
+        </el-alert>
+      </el-form-item>
+      <el-form-item label="任务描述">
+        <el-input type="textarea" v-model="scheduleJob.jobDescription"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="handlerAdd">添加</el-button>
+      <el-button size="mini" @click="formStatus.visible = false">关闭</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import {
+  ADD_DATASOURCE_INSTANCE_SCHEDULE,
+  DATASOURCE_INSTANCE_SCHEDULE_CRON_CHECK
+} from '@/api/modules/datasource/datasource.schedule.api'
+
+const scheduleJob = {
+  jobType: 'AssetPullJob',
+  instanceId: '',
+  assetType: '',
+  jobTime: ''
+}
+
+export default {
+  name: 'DsInstanceScheduleAddEditor',
+  props: ['formStatus'],
+  data () {
+    return {
+      jobTypeOptions: [{
+        value: 'AssetPullJob',
+        label: '资产拉取'
+      }],
+      assetTypeOptions: [],
+      scheduleJob: {},
+      executionTime: [],
+      showExecutionTime: false,
+      cronChecked: false
+    }
+  },
+  mixins: [],
+  components: {},
+  methods: {
+    initData (instance) {
+      this.scheduleJob = Object.assign({}, scheduleJob)
+      this.scheduleJob.instanceId = instance.id
+      this.assetTypeOptions = instance.assetDetails
+      this.executionTime = []
+      this.showExecutionTime = false
+      this.cronChecked = false
+    },
+    handlerChange () {
+      this.scheduleJob.jobTime = this.scheduleJob.jobTime.trim()
+    },
+    handlerAdd () {
+      if (!this.cronChecked) {
+        return this.$message.warning('请先校验Cron表达式')
+      }
+      ADD_DATASOURCE_INSTANCE_SCHEDULE(this.scheduleJob)
+        .then(() => {
+          this.$message.success('新增任务成功')
+          this.formStatus.visible = false
+          this.$emit('close')
+        })
+    },
+    handleCheck () {
+      this.executionTime = []
+      DATASOURCE_INSTANCE_SCHEDULE_CRON_CHECK({ jobTime: this.scheduleJob.jobTime.trim() })
+        .then(({ body }) => {
+          if (JSON.stringify(body) === '[]') {
+            return this.$message.error('Cron表达式异常')
+          }
+          this.executionTime = body
+          this.showExecutionTime = true
+          this.cronChecked = true
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/opscloud/datasource/DsInstanceScheduleEditor.vue
+++ src/components/opscloud/datasource/DsInstanceScheduleEditor.vue
@@ -1,58 +1,127 @@
 <template>
   <el-dialog title="编辑数据源实例动态分布式任务" :visible.sync="formStatus.visible">
-    <el-form :model="instanceSchedule">
-      <el-table :data="instanceSchedule.schedules" style="width: 100%">
-        <el-table-column prop="name" label="任务名称" width="100"></el-table-column>
-        <el-table-column prop="cronExpression" label="CRON表达式" width="100">
-          <template slot-scope="scope">
-            <span>{{ scope.row.cronExpression }}</span>
-            <el-popover placement="right" trigger="hover" title="最近5次执行时间">
-              <i class="el-icon-info" style="color: green;margin-left: 5px" slot="reference"></i>
-              <div v-for="time in scope.row.executionTime" :key="time">{{ time }}</div>
-            </el-popover>
-          </template>
-        </el-table-column>
-        <el-table-column prop="status" label="状态" width="80">
-          <template slot-scope="scope">
-            <el-tag>{{ scope.row.status }}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="description" label="描述" width="120"></el-table-column>
-        <el-table-column label="操作" width="80">
-          <template slot-scope="scope">
-            <el-button type="danger" plain size="mini" @click="handleDelSchedule(scope.row)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-
-    </el-form>
+    <div style="margin-bottom: 5px">
+      <el-button @click="handlerAdd" size="mini" plain>新增</el-button>
+    </div>
+    <el-table :data="schedules" style="width: 100%" v-loading="loading">
+      <el-table-column prop="name" label="任务名称" width="100"></el-table-column>
+      <el-table-column prop="cronExpression" label="CRON表达式" width="100">
+        <template slot-scope="scope">
+          <span>{{ scope.row.cronExpression }}</span>
+          <el-popover placement="right" trigger="hover" title="最近5次执行时间">
+            <i class="el-icon-info" style="color: green;margin-left: 5px" slot="reference"></i>
+            <div v-for="time in scope.row.executionTime" :key="time">{{ time }}</div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column prop="status" label="状态" width="80">
+        <template slot-scope="scope">
+          <el-tag>{{ scope.row.status }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="description" label="描述" width="120"></el-table-column>
+      <el-table-column label="操作" width="80">
+        <template slot-scope="scope">
+          <el-button-group>
+            <el-button type="primary" size="mini" plain @click="handleScheduleResume(scope.row)"
+                       v-if="scope.row.status === 'PAUSED'">恢复
+            </el-button>
+            <el-button type="primary" size="mini" plain @click="handleSchedulePause(scope.row)"
+                       v-if="scope.row.status === 'NORMAL'">暂停</el-button>
+            <el-button type="danger" size="mini" plain @click="handleScheduleDel(scope.row)">删除</el-button>
+          </el-button-group>
+        </template>
+      </el-table-column>
+    </el-table>
     <div slot="footer" class="dialog-footer">
       <el-button size="mini" @click="formStatus.visible = false">关闭</el-button>
     </div>
+    <ds-instance-schedule-add-editor :form-status="addEditorStatus" @close="fetchData"
+                                     ref="dsInstanceScheduleAddEditor"></ds-instance-schedule-add-editor>
   </el-dialog>
 </template>
 
 <script>
+import DsInstanceScheduleAddEditor from '@/components/opscloud/datasource/DsInstanceScheduleAddEditor'
+import {
+  DELETE_DATASOURCE_INSTANCE_SCHEDULE, PAUSE_DATASOURCE_INSTANCE_SCHEDULE,
+  QUERY_DATASOURCE_INSTANCE_SCHEDULE_BY_ID, RESUME_DATASOURCE_INSTANCE_SCHEDULE
+} from '@/api/modules/datasource/datasource.schedule.api'
+
 export default {
   name: 'DsInstanceScheduleEditor',
   props: ['formStatus'],
   data () {
     return {
       labelWidth: '150px',
-      instanceSchedule: {
-        schedules: [],
-        instance: {}
-      }
+      schedules: [],
+      instance: {},
+      addEditorStatus: {
+        visible: false
+      },
+      loading: false
     }
   },
-  mixins: [],
-  components: {},
+  components: {
+    DsInstanceScheduleAddEditor
+  },
   methods: {
-    initData (instanceSchedule) {
-      this.instanceSchedule = instanceSchedule
+    initData (instance) {
+      this.instance = Object.assign({}, instance)
+      this.fetchData()
     },
-    handleDelSchedule (row) {
-
+    fetchData () {
+      this.loading = true
+      QUERY_DATASOURCE_INSTANCE_SCHEDULE_BY_ID({ id: this.instance.id })
+        .then(({ body }) => {
+          this.schedules = body
+          this.loading = false
+        })
+    },
+    handlerAdd () {
+      this.$refs.dsInstanceScheduleAddEditor.initData(this.instance)
+      this.addEditorStatus.visible = true
+    },
+    handleScheduleDel (row) {
+      this.$confirm('此操作将删除当前配置?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        const requestBody = {
+          group: row.group,
+          name: row.name
+        }
+        DELETE_DATASOURCE_INSTANCE_SCHEDULE(requestBody)
+          .then(() => {
+            this.fetchData()
+            this.$message.success('删除成功!')
+          })
+      }).catch(() => {
+        this.$message.info('已取消删除!')
+      })
+    },
+    handleScheduleResume (row) {
+      const requestBody = {
+        group: row.group,
+        name: row.name
+      }
+      RESUME_DATASOURCE_INSTANCE_SCHEDULE(requestBody)
+        .then(() => {
+          this.fetchData()
+          this.$message.success('恢复成功!')
+        })
+    },
+    handleSchedulePause (row) {
+      const requestBody = {
+        group: row.group,
+        name: row.name
+      }
+      PAUSE_DATASOURCE_INSTANCE_SCHEDULE(requestBody)
+        .then(() => {
+          this.fetchData()
+          this.$message.success('暂停成功!')
+        })
     }
   }
 }
