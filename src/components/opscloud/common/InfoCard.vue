fe7ec01173966a88a0bd63be1c94a23257b527a5
白衣 <ixrjog@qq.com>
2022-07-18 09:28:15 +0800

    堡垒机审计报表数字使用千分位

--- src/components/opscloud/common/InfoCard.vue
+++ src/components/opscloud/common/InfoCard.vue
@@ -5,7 +5,7 @@
         <span>{{ title }}</span>
         <el-tag style="float: right" type="text">{{tag}}</el-tag>
       </div>
-      <div style="font-size: 45px;color:#20A9D9 ;font-weight: 300">{{ content }}</div>
+      <div style="font-size: 40px;color:#20A9D9 ;font-weight: 300">{{ content.toLocaleString() }}</div>
       <div style="font-size: 10px; color: #B7B6B6">{{ footer }}</div>
     </el-card>
   </div>
