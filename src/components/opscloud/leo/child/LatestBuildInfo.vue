daf8bb5788796f9a4d31c330bb0065e37cc09cc6
白衣 <ixrjog@qq.com>
2022-12-24 20:06:53 +0800

    删除提示

--- src/components/opscloud/leo/child/LatestBuildInfo.vue
+++ src/components/opscloud/leo/child/LatestBuildInfo.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <span v-for="item in buildInfos" :key="item.buildNumber">
-      <el-popover placement="top-start" trigger="hover">
-        <span slot="reference">
+       <span slot="reference">
             <el-tag :style="{ backgroundColor: item.color, color: '#FFFFFF',width: '50px' }">
               <i class="el-icon-loading" v-if="item.running"></i>{{ item.buildNumber }}</el-tag>
         </span>
-      </el-popover>
+<!--      <el-popover placement="top-start" trigger="hover">-->
+<!--        <span slot="reference">-->
+<!--            <el-tag :style="{ backgroundColor: item.color, color: '#FFFFFF',width: '50px' }">-->
+<!--              <i class="el-icon-loading" v-if="item.running"></i>{{ item.buildNumber }}</el-tag>-->
+<!--        </span>-->
+<!--      </el-popover>-->
     </span>
   </div>
 </template>
