20d3610de409951f7263592b21ce02dde3e41bd0
白衣 <ixrjog@qq.com>
2023-01-31 10:20:13 +0800

    用户终端设置改成白底黑字，并使用英文

--- src/components/opscloud/user/UserTerminalSetting.vue
+++ src/components/opscloud/user/UserTerminalSetting.vue
@@ -1,19 +1,19 @@
 <template>
   <el-dialog :title="title" :visible.sync="formStatus.visible" width="30%">
     <el-form :model="terminalSetting" :label-width="labelWidth">
-      <el-form-item label="终端字体颜色">
+      <el-form-item label="Foreground">
         <el-color-picker v-model="terminalSetting.theme.foreground"></el-color-picker>
       </el-form-item>
-      <el-form-item label="终端背景颜色">
+      <el-form-item label="Background">
         <el-color-picker v-model="terminalSetting.theme.background"></el-color-picker>
       </el-form-item>
-      <el-form-item label="终端行数">
+      <el-form-item label="Windows Rows">
         <el-input-number v-model="terminalSetting.rows" :min="22" :max="60" label="终端行数"></el-input-number>
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
-      <el-button size="mini" @click="formStatus.visible = false">取消</el-button>
-      <el-button size="mini" type="primary" @click="handlerSave">确定</el-button>
+      <el-button size="mini" @click="formStatus.visible = false">Cancel</el-button>
+      <el-button size="mini" type="primary" @click="handlerSave">Save</el-button>
     </div>
   </el-dialog>
 </template>
@@ -24,12 +24,12 @@ import { mapActions, mapState } from 'vuex'
 export default {
   data () {
     return {
-      title: '用户终端设置',
+      title: 'User Profiles',
       labelWidth: '150px',
       terminalSetting: {
         theme: {
           foreground: '#FFFFFF', // 字体
-          background: '#606266' // 背景色
+          background: '#090909' // 背景色
         },
         rows: 30 // 行数
       }
