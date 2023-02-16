b14b9a950c77725b90f473ca4c4404dd4d33a0a6
白衣 <ixrjog@qq.com>
2023-02-16 17:22:04 +0800

    部署详情

--- src/components/opscloud/leo/LeoDeployTable.vue
+++ src/components/opscloud/leo/LeoDeployTable.vue
@@ -23,10 +23,10 @@
           <select-item :name="item.envName" :comment="item.comment"></select-item>
         </el-option>
       </el-select>
-      <el-select v-model="queryParam.buildResult" clearable filterable placeholder="选择构建结果"
+      <el-select v-model="queryParam.deployResult" clearable filterable placeholder="选择部署结果"
                  @change="fetchData">
         <el-option
-          v-for="item in buildResultOptions"
+          v-for="item in deployResultOptions"
           :key="item.value"
           :label="item.label"
           :value="item.value">
@@ -34,55 +34,21 @@
       </el-select>
       <el-button @click="fetchData" class="button">刷新</el-button>
     </el-row>
-
-    <div v-for="build in table.data" :key="build.id" style="font-size: 12px">
+    <div v-for="deploy in table.data" :key="deploy.id" style="font-size: 12px">
       <template>
         <div>
-          <leo-build-details :build="build" :ref="`leoBuildDetails_${build.id}`"></leo-build-details>
+          <leo-deploy-details :deploy="deploy" :ref="`leoDeployDetails_${deploy.id}`"></leo-deploy-details>
         </div>
       </template>
     </div>
-
-    <el-table :data="table.data" style="width: 100%" v-loading="table.loading" v-if="false">
-      <el-table-column prop="name" label="名称" sortable></el-table-column>
-      <el-table-column prop="branch" label="首选分支" sortable>
-        <template v-slot="scope">
-          <i class="fa fa-code-fork" style="margin-right: 2px"></i>
-          <span>{{ scope.row.branch }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="env" label="环境" width="80">
-        <template v-slot="scope">
-          <env-tag :env="scope.row.env"></env-tag>
-        </template>
-      </el-table-column>
-      <el-table-column prop="buildSize" label="构建次数" width="80">
-      </el-table-column>
-      <el-table-column prop="tags" label="标签" width="200">
-        <template v-slot="scope">
-          <business-tags :tags="scope.row.tags"></business-tags>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" width="200">
-        <template v-slot="scope">
-          <el-button type="primary" plain size="mini" @click="handleBuild(scope.row)">构建</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
     <pagination :pagination="table.pagination" @paginationCurrentChange="paginationCurrentChange"
                 @handleSizeChange="handleSizeChange"></pagination>
-    <leo-job-editor :formStatus="formStatus.job" ref="jobEditor"
-                    @close="fetchData"></leo-job-editor>
-    <leo-do-build-editor :form-status="formStatus.build" ref="doBuildEditor">
-    </leo-do-build-editor>
-    <business-tag-editor ref="businessTagEditor" :business-type="businessType" :business-id="instance.id"
-                         :form-status="formStatus.businessTag" @close="fetchData"></business-tag-editor>
   </div>
 </template>
 
 <script>
 
-import { DELETE_LEO_JOB_BY_ID, QUERY_LEO_JOB_BUILD_PAGE } from '@/api/modules/leo/leo.job.api'
+import { QUERY_LEO_JOB_DEPLOY_PAGE } from '@/api/modules/leo/leo.job.api'
 import { QUERY_MY_APPLICATION_PAGE } from '@/api/modules/application/application.api'
 import { QUERY_TAG_PAGE } from '@/api/modules/tag/tag.api.js'
 import { QUERY_ENV_PAGE } from '@/api/modules/sys/sys.env.api'
@@ -93,10 +59,9 @@ import BusinessTagEditor from '../common/tag/BusinessTagEditor'
 import Pagination from '../common/page/Pagination'
 import BusinessType from '@/components/opscloud/common/enums/business.type.js'
 import EnvTag from '@/components/opscloud/common/tag/EnvTag'
-import { QUERY_LEO_TEMPLATE_PAGE } from '@/api/modules/leo/leo.template.api'
 import LeoJobEditor from '@/components/opscloud/leo/LeoJobEditor'
 import LeoDoBuildEditor from '@/components/opscloud/leo/LeoDoBuildEditor'
-import LeoBuildDetails from '@/components/opscloud/leo/LeoBuildDetails.vue'
+import LeoDeployDetails from '@/components/opscloud/leo/LeoDeployDetails.vue'
 
 const activeOptions = [{
   value: true,
@@ -106,23 +71,15 @@ const activeOptions = [{
   label: '无效'
 }]
 
-const buildResultOptions = [{
-  value: 'SUCCESS',
-  label: 'SUCCESS'
-}, {
-  value: 'ABORTED',
-  label: 'ABORTED'
-}, {
-  value: 'FAILURE',
-  label: 'FAILURE'
-}, {
-  value: 'UNSTABLE',
-  label: 'UNSTABLE'
-}, {
-  value: 'ERROR',
-  label: 'ERROR'
-}
-
+const deployResultOptions = [
+  {
+    value: 'SUCCESS',
+    label: 'SUCCESS'
+  },
+  {
+    value: 'ERROR',
+    label: 'ERROR'
+  }
 ]
 
 export default {
@@ -152,36 +109,26 @@ export default {
           operationType: true,
           addTitle: '新增任务配置',
           updateTitle: '更新任务配置'
-        },
-        build: {
-          visible: false,
-          labelWidth: '150px'
         }
       },
       queryParam: {
         queryName: '',
         applicationId: '',
-        templateId: '',
-        envType: '',
-        tagId: '',
-        buildResult: '',
-        isActive: '',
-        extend: true
+        deployResult: '',
+        envType: ''
       },
       tagOptions: [],
       businessType: BusinessType.LEO_JOB,
       applicationOptions: [],
       templateOptions: [],
       envOptions: [],
-      buildResultOptions: buildResultOptions,
+      deployResultOptions: deployResultOptions,
       activeOptions: activeOptions
     }
   },
   mounted () {
     this.fetchData()
     this.getApplication('')
-    this.getTemplate('')
-    this.getTag('')
     this.getEnv()
   },
   computed: {},
@@ -193,7 +140,7 @@ export default {
     BusinessTagEditor,
     LeoJobEditor,
     LeoDoBuildEditor,
-    LeoBuildDetails
+    LeoDeployDetails
   },
   filters: {},
   methods: {
@@ -217,19 +164,6 @@ export default {
           this.envOptions = res.body.data
         })
     },
-    getTag (name) {
-      const requestBody = {
-        tagKey: name,
-        businessType: this.businessType,
-        append: true,
-        page: 1,
-        length: 20
-      }
-      QUERY_TAG_PAGE(requestBody)
-        .then(res => {
-          this.tagOptions = res.body.data
-        })
-    },
     getApplication (name) {
       const requestBody = {
         queryName: name,
@@ -242,71 +176,6 @@ export default {
           this.applicationOptions = res.body.data
         })
     },
-    getTemplate (name) {
-      const requestBody = {
-        queryName: name,
-        extend: false,
-        page: 1,
-        length: 20
-      }
-      QUERY_LEO_TEMPLATE_PAGE(requestBody)
-        .then(res => {
-          this.templateOptions = res.body.data
-        })
-    },
-    handleRowTagEdit (row) {
-      this.instance.id = row.id
-      const businessTags = {
-        tagIds: row.tags !== null ? row.tags.map(e => e.id) : []
-      }
-      this.$refs.businessTagEditor.initData(businessTags)
-      this.formStatus.businessTag.visible = true
-    },
-    handleAdd () {
-      this.formStatus.job.visible = true
-      this.formStatus.job.operationType = true
-      const job = {
-        id: '',
-        parentId: 0,
-        applicationId: '',
-        name: '',
-        jobKey: '',
-        branch: '',
-        envType: 0,
-        jobConfig: '',
-        buildNumber: 0,
-        templateId: '',
-        templateVersion: '',
-        hide: false,
-        href: '',
-        isActive: true,
-        comment: ''
-      }
-      this.$refs.jobEditor.initData(job)
-    },
-    handleRowEdit (row) {
-      this.formStatus.job.visible = true
-      this.formStatus.job.operationType = false
-      this.$refs.jobEditor.initData(Object.assign({}, row))
-    },
-    handleRowDel (row) {
-      this.$confirm('此操作将删除当前配置?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        DELETE_LEO_JOB_BY_ID({ jobId: row.id }).then(res => {
-          this.$message.success('删除成功!')
-          this.fetchData()
-        })
-      }).catch(() => {
-        this.$message.info('已取消删除!')
-      })
-    },
-    handleBuild (row) {
-      this.formStatus.build.visible = true
-      this.$refs.doBuildEditor.initData(Object.assign({}, row))
-    },
     fetchData () {
       if (this.queryParam.applicationId === '') return
       this.table.loading = true
@@ -315,7 +184,7 @@ export default {
         page: this.table.pagination.currentPage,
         length: this.table.pagination.pageSize
       }
-      QUERY_LEO_JOB_BUILD_PAGE(requestBody)
+      QUERY_LEO_JOB_DEPLOY_PAGE(requestBody)
         .then(res => {
           this.table.data = res.body.data
           this.table.pagination.total = res.body.totalNum
