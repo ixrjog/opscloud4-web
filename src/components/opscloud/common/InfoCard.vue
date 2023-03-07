e3d09895f8fa5a2e0c1f189abc405671a99cb145
白衣 <ixrjog@qq.com>
2023-03-07 17:59:08 +0800

    报表优化

--- src/components/opscloud/common/InfoCard.vue
+++ src/components/opscloud/common/InfoCard.vue
@@ -2,10 +2,11 @@
   <div>
     <el-card shadow="hover">
       <div>
-        <span style="color: #9d9fa3">{{ title }}</span>
+        <b class="title">{{ title }}</b>
         <el-tag v-if="false" style="float: right" type="text">{{ tag }}</el-tag>
       </div>
-      <div style="font-size: 40px;color:#20A9D9 ;font-weight: 300">{{ content > 0 ? content.toLocaleString() :0}}</div>
+      <div class="value">{{ content > 0 ? content.toLocaleString() : 0 }}
+      </div>
       <div v-show="false" style="font-size: 10px; color: #B7B6B6">{{ footer }}</div>
     </el-card>
   </div>
@@ -24,5 +25,14 @@ export default {
   border-bottom: 1px solid #EBEEF5;
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
+}
+
+.title {
+  color: #9d9fa3
+}
+.value {
+  font-size: 40px;
+  color: #20A9D9;
+  font-weight: 300;
 }
 </style>
