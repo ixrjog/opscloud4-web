380612768683b776cca29d4ac4c8b1687cf1b402
白衣 <ixrjog@qq.com>
2023-03-21 16:09:07 +0800

    环境终端提示色修改

--- src/components/opscloud/common/tag/PromptColorTag.vue
+++ src/components/opscloud/common/tag/PromptColorTag.vue
@@ -1,9 +1,11 @@
 <template>
-  <el-tag disable-transitions size="mini"
-          :style="{ backgroundColor: promptColorOptions.find(e=> e.value === env.promptColor).label }">
-    <span :style="{color: util.colorReverse(env.color)}">
-          {{ promptColorOptions.find(e => e.value === env.promptColor).label }}
-    </span>
+<!--  <el-tag disable-transitions size="mini"-->
+<!--          :style="{ backgroundColor: promptColorOptions.find(e=> e.value === env.promptColor).label }">-->
+<!--    <i class="fas fa-square" :style="{color: util.colorReverse(env.color)}"></i>{{ promptColorOptions.find(e => e.value === env.promptColor).label }}-->
+<!--  </el-tag>-->
+  <el-tag disable-transitions size="mini">
+    <i class="fas fa-square" :style="{color: promptColorOptions.find(e=> e.value === env.promptColor).label}"></i>
+    <span style="margin-left: 2px">{{ promptColorOptions.find(e => e.value === env.promptColor).label }}</span>
   </el-tag>
 </template>
 
