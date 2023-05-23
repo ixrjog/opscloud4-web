b93df8c4d44187ab668fc75c2516d0ba7413db9f
白衣 <ixrjog@qq.com>
2023-05-23 17:48:07 +0800

    优化ssh账户显示

--- src/components/opscloud/common/tag/AccountTags.vue
+++ src/components/opscloud/common/tag/AccountTags.vue
@@ -1,11 +1,13 @@
 <template>
   <span class="tag-group">
     <span v-for="item in accounts" :key="item.id">
-        <el-tag size="mini" style="margin-left: 5px" :style="{ color: item.accountType ===0 ? '#67c23a' : '#F56C6C' }">
+        <el-tag size="mini" style="margin-left: 5px" :style="{ color: item.accountType === 0 ? '#67c23a' : '#F56C6C' }">
           <i class="fas fa-desktop" aria-hidden="true" v-show="item.protocol=== 'vnc'"></i>
-          <i class="fas fa-terminal" aria-hidden="true" v-show="item.protocol=== 'ssh'"></i>
+<!--          <i class="fas fa-terminal" aria-hidden="true" v-show="item.protocol=== 'ssh'"></i>-->
           <i class="fab fa-windows" aria-hidden="true" v-show="item.protocol=== 'rdp'"></i>
-          {{ item.username }}</el-tag>
+           {{ item.username }}
+          <span v-show="item.protocol=== 'ssh'">{{ item.accountType === 0 ? '$': '#'}}</span>
+        </el-tag>
     </span>
   </span>
 </template>
