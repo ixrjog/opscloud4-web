ebe35532f4b670e6ecd6cc6a20b30ab28b31484d
白衣 <ixrjog@qq.com>
2023-06-15 20:15:09 +0800

    fixbug

--- src/components/opscloud/user/child/UserInfo.vue
+++ src/components/opscloud/user/child/UserInfo.vue
@@ -7,7 +7,7 @@
       </el-form-item>
       <el-form-item label="密码" :label-width="labelWidth" :required="operationType">
         <el-input v-model="user.password" clearable placeholder="请输入内容">
-          <el-button v-slot:append @click="handleRandomWord">
+          <el-button slot="append" @click="handleRandomWord">
             <i class="fas fa-key" aria-hidden="true"/>
           </el-button>
         </el-input>
