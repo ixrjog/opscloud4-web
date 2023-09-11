29b273cc8a9618242fe763f71ada504eb6cd7fae
白衣 <ixrjog@qq.com>
2023-09-11 15:30:21 +0800

    application active i18n

--- src/components/opscloud/common/tag/ActiveTag.vue
+++ src/components/opscloud/common/tag/ActiveTag.vue
@@ -1,7 +1,7 @@
 <!--suppress HtmlUnknownTag -->
 <template>
   <el-tag size="mini" disable-transitions :type="isActive ? 'success' : 'info'">
-    {{ isActive ? '有效' : '无效' }}
+    {{ isActive ? $t('common.activeOptions.active') : $t('common.activeOptions.inactive') }}
   </el-tag>
 </template>
 
